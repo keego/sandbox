0cc880ada992a346e01de39a354c7a02c7393d58
Keegan Farley <keegomyneego@live.com>
2018-10-24 18:48:24 -0700

    ? added Vue ActionButton - likely testing out vue-property-decorator

---
+++ Vue/components/src/components/ActionButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      saving to cloud
+    </div>
+    <div>
+      Save
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ActionButton extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped>
+.loading {
+
+}
+</style>
