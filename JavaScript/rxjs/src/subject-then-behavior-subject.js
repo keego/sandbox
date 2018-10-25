12366fce147903fbc05be7092ed724a16ad8fc66
Keegan Farley <keegomyneego@live.com>
2018-10-24 19:23:17 -0700

    updates to subject-then-bs

--- JavaScript/rxjs/src/subject-then-behavior-subject.js
+++ JavaScript/rxjs/src/subject-then-behavior-subject.js
@@ -1,28 +1,43 @@
 const Rx = require('rxjs')
 
 
-const aInfoSource$ = new Rx.Subject()
-const aInfo$ = aInfoSource$.publishReplay(1).refCount()
+const aInfoSource$ = new Rx.ReplaySubject(1)
+const aInfo$ = aInfoSource$
+const subs = []
 
-aInfo$.subscribe(info => {
+aInfoSource$.next({ lots: 'init', account: 'init' })
+
+subs.push(aInfo$.subscribe(info => {
   console.log('got account info 1', info)
-})
+}))
 
 setTimeout(() => {
+  // info 1 isnt called until now
   aInfoSource$.next({ lots: 'of', account: 'info' })
 
   setTimeout(() => {
-    aInfo$.subscribe(info => {
+    // info 2 called immediately
+    subs.push(aInfo$.subscribe(info => {
       console.log('got account info 2', info)
-    })
+    }))
 
     setTimeout(() => {
+      // both subs called
       aInfoSource$.next({ lots: 'and lots of', account: 'info' })
 
       setTimeout(() => {
-        aInfo$.subscribe(info => {
+        // info 3 only gets latest value (not any previous)
+        subs.push(aInfo$.subscribe(info => {
           console.log('got account info 3', info)
-        })
+        }))
+
+        setTimeout(() => {
+          // info 4 should get the same value as info 3
+          subs.forEach(sub => sub.unsubscribe())
+          aInfo$.subscribe(info => {
+            console.log('got account info 4', info)
+          })
+        }, 1000)
       }, 1000)
     }, 1000)
   }, 1000)
